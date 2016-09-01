b5284506d16d945292819288e6e3e55afab52d4d
antonia devina <antonia.devina78@gmail.com>
2016-09-01 10:04:07 +0800

    Split address storage into block,street,unit and postal code

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -1,5 +1,7 @@
 package seedu.addressbook.data.person;
 
+import java.util.regex.Pattern; 
+import java.util.regex.Matcher;
 import seedu.addressbook.data.exception.IllegalValueException;
 
 /**
@@ -8,11 +10,17 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
-
-    public final String value;
+    public static final String EXAMPLE = "123, Clementi Ave 3, #12-34, 231534";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses should be in this format: a/BLOCK, STREET, UNIT, POSTAL_CODE ";
+    public static final String ADDRESS_VALIDATION_REGEX = "(?<block>[^/]+)," + " (?<street>[^/]+)," 
+			+ " (?<unit>[^/]+)," + " (?<postalCode>[^/]+)";
+    private static final Pattern ADDRESS_PATTERN = Pattern.compile(ADDRESS_VALIDATION_REGEX);
+    
+ //   public final String completeAddress;
+    private final String block; 
+    private final String street; 
+    private final String unit; 
+    private final String postalCode; 
     private boolean isPrivate;
 
     /**
@@ -22,34 +30,39 @@ public class Address {
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
         this.isPrivate = isPrivate;
-        if (!isValidAddress(address)) {
+        Matcher addressMatch = ADDRESS_PATTERN.matcher(address);
+        if (!isValidAddress(addressMatch)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = address;
+//        this.completeAddress = address;
+        this.block = addressMatch.group("block");
+        this.street = addressMatch.group("street");
+        this.unit = addressMatch.group("unit");
+        this.postalCode = addressMatch.group("postalCode");
     }
 
     /**
      * Returns true if a given string is a valid person email.
      */
-    public static boolean isValidAddress(String test) {
-        return test.matches(ADDRESS_VALIDATION_REGEX);
+    public static boolean isValidAddress(Matcher test) {
+        return test.matches();
     }
 
     @Override
     public String toString() {
-        return value;
+        return block + ", " + street + ", " + unit + ", " + postalCode;
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+                && this.toString().equals(((Address) other).toString())); // state check
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return toString().hashCode();
     }
 
     public boolean isPrivate() {
